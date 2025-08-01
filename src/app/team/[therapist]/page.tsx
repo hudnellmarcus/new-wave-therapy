e8c3a688acf109d8bb4fad46b0130369f3592e43
Marcus Hudnell <103913070+hudnellmarcus@users.noreply.github.com>
2025-08-01 09:22:14 -0700

    Clean up project structure and organize components
    - Remove duplicate About directory
    - Move background images to /backgrounds/ folder and update all references
    - Clean up deleted team member components and fix imports
    - Rename team-member-options to team-member-layouts for better organization
    - Keep 3 working team member layouts (5, 6, 8) with clear documentation

--- src/app/team/[therapist]/page.tsx
+++ src/app/team/[therapist]/page.tsx
@@ -1,23 +1,16 @@
-import TeamMemberOption1 from "../../components/team-member-options/TeamMemberOption1";
-import TeamMemberOption2 from "../../components/team-member-options/TeamMemberOption2";
-import TeamMemberOption3 from "../../components/team-member-options/TeamMemberOption3";
-import TeamMemberOption4 from "../../components/team-member-options/TeamMemberOption4";
-import TeamMemberOption5 from "../../components/team-member-options/TeamMemberOption5";
-import TeamMemberOption6 from "../../components/team-member-options/TeamMemberOption6";
-import TeamMemberOption7 from "../../components/team-member-options/TeamMemberOption7";
-import TeamMemberOption8 from "../../components/team-member-options/TeamMemberOption8";
+import TeamMemberOption5 from "../../components/team-member-layouts/TeamMemberOption5";
+import TeamMemberOption6 from "../../components/team-member-layouts/TeamMemberOption6";
+import TeamMemberOption8 from "../../components/team-member-layouts/TeamMemberOption8";
 
 const TherapistPage = ({ params }: { params: { therapist: string } }) => {
   return (
     <>
        
-       {/* kind of like the specializations display of option 3 */}
-       <TeamMemberOption3 therapistName={params.therapist} /> - Dark modern layout
-       <TeamMemberOption4 therapistName={params.therapist} /> - Dark vintage centered layout
+       {/* Available Options for Testing:
        <TeamMemberOption5 therapistName={params.therapist} /> - Split layout with photo on side
-      <TeamMemberOption6 therapistName={params.therapist} /> - Creative tilted photo with floating tags 
-      <TeamMemberOption7 therapistName={params.therapist} /> - Clean flowing layout with photo/contact split
-      <TeamMemberOption8 therapistName={params.therapist} /> {/* v0.dev retro design with full-screen layout */}
+       <TeamMemberOption6 therapistName={params.therapist} /> - Creative tilted photo with floating tags
+       <TeamMemberOption8 therapistName={params.therapist} /> - v0.dev retro design with full-screen layout */}
+      <TeamMemberOption8 therapistName={params.therapist} />
     </>
   );
 };
